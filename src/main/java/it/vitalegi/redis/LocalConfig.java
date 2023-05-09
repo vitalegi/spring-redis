fa92603b29600bacab6a6c8d603657cd96e7bb82
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-05-09 21:20:09 +0200

    cache using ConcurrentHashMap if running locally

--- src/main/java/it/vitalegi/redis/LocalConfig.java
+++ src/main/java/it/vitalegi/redis/LocalConfig.java
@@ -1,13 +1,15 @@
 package it.vitalegi.redis;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import redis.embedded.RedisServer;
+
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Profile("local")
 @Configuration
@@ -15,26 +17,11 @@ import redis.embedded.RedisServer;
 public class LocalConfig {
 
     @Bean
-    LettuceConnectionFactory redisConnectionFactory(@Value("${redis.host}") String host,
-                                                    @Value("${redis.port}") int port) {
-        log.info("Init LettuceConnectionFactory {}:{}", host, port);
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    @Bean
-    RedisServer redisServer(@Value("${redis.port}") int port) {
-        log.info("Initialize dummy RedisServer");
-        var redisServer = new RedisServer(port);
-        redisServer.start();
-        return redisServer;
-    }
-
-    @Bean
-    RedisTemplate<?, ?> redisTemplate(LettuceConnectionFactory connectionFactory) {
-        log.info("Init redisTemplate with lettuce");
-        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        return template;
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        var userCache = new ConcurrentMapCache("userCache", new ConcurrentHashMap<>(), true);
+        cacheManager.setCaches(Arrays.asList(userCache));
+        return cacheManager;
     }
 
 }
--- src/main/java/it/vitalegi/redis/RemoteConfig.java
+++ src/main/java/it/vitalegi/redis/RemoteConfig.java
@@ -2,16 +2,36 @@ package it.vitalegi.redis;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @Profile("remote")
 @Configuration
 @Slf4j
 public class RemoteConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(60)).disableCachingNullValues()
+                                      .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder.withCacheConfiguration("userCache", RedisCacheConfiguration.defaultCacheConfig()
+                                                                                               .entryTtl(Duration.ofSeconds(10)))
+                                   .withCacheConfiguration("customerCache", RedisCacheConfiguration.defaultCacheConfig()
+                                                                                                   .entryTtl(Duration.ofSeconds(5)));
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
