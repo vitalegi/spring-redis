47ac778f39a6ce702c03a46cba968833a407f5d8
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-05-09 20:12:19 +0200

    init

---
+++ src/test/java/it/vitalegi/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vitalegi.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
